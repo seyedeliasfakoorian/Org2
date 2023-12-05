fd615d7e64eb2094127164f7d14c8f9519202a9a
Elias Fakoorian <98291494+seyedeliasfakoorian@users.noreply.github.com>
2023-12-04 19:32:01 -0800

    bug fixes

---
+++ app.php
@@ -0,0 +1,46 @@
+<?php
+
+// Load the head image
+$headImage = imagecreatefrompng('public/favicon.png');
+
+// Get image dimensions
+$width = imagesx($headImage);
+$height = imagesy($headImage);
+
+// Calculate the width of each piece
+$pieceWidth = $width / 3;
+
+// Array to store image sources
+$imageSources = [];
+
+// Loop through and split the image into 12 pieces
+for ($i = 0; $i < 3; $i++) {
+    for ($j = 0; $j < 4; $j++) {
+        // Create a new image for each piece
+        $piece = imagecreatetruecolor($pieceWidth, $height / 4);
+
+        // Copy a portion of the original image to the new piece
+        imagecopy($piece, $headImage, 0, 0, $i * $pieceWidth, $j * ($height / 4), $pieceWidth, $height / 4);
+
+        // Save the piece to a variable (base64 encoded)
+        ob_start();
+        imagepng($piece);
+        $imageData = ob_get_contents();
+        ob_end_clean();
+
+        // Encode the image data in base64
+        $base64Image = 'data:image/png;base64,' . base64_encode($imageData);
+
+        // Push the base64 encoded image to the array
+        $imageSources[] = $base64Image;
+    }
+}
+
+// Output HTML with image sources
+echo '<div class="image-gallery">';
+foreach ($imageSources as $index => $src) {
+    echo '<img src="' . $src . '" alt="Piece ' . $index . '">';
+}
+echo '</div>';
+
+?>
