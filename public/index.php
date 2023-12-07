d6453f3d15af20b96a202fb55db6840af7976630
Elias Fakoorian <98291494+seyedeliasfakoorian@users.noreply.github.com>
2023-12-06 18:01:26 -0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -44,9 +44,9 @@ for ($i = 0; $i < 3; $i++) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="%PUBLIC_URL%/styles.css" />
+    <link rel="stylesheet" href="styles.css" />
     <title>React-App</title>
-    <!-- Include jQuery from CDN -->
+    <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI/tZ1XaIBlEYF8eVv9YHdL7RoCZ+ZlEi1R9eHoI=" crossorigin="anonymous"></script>
 </head>
 <body>
