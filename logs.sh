4ffe5202771b9353c4c2f26978a55056163b02d1
Elias Fakoorian <98291494+seyedeliasfakoorian@users.noreply.github.com>
2023-12-23 03:11:05 +0000

    Update logs.sh

--- logs.sh
+++ logs.sh
@@ -6,9 +6,6 @@ if ! command -v heroku &> /dev/null; then
     exit 1
 fi
 
-# Print all environment variables for debugging
-env
-
 # Check if required environment variables are set
 if [ -z "$HEROKU_APP_NAME" ]; then
     echo "Error: HEROKU_APP_NAME environment variable must be set."
@@ -20,13 +17,10 @@ if [ -z "$HEROKU_API_KEY" ]; then
     exit 1
 fi
 
-# Print the values of HEROKU_APP_NAME and HEROKU_API_KEY for debugging
-echo "HEROKU_APP_NAME: $HEROKU_APP_NAME"
-echo "HEROKU_API_KEY: $HEROKU_API_KEY"
-
 # Log in using Heroku API key
 echo "Logging in to Heroku..."
-if ! heroku auth:token > /dev/null 2>&1; then
+heroku auth:token > /dev/null 2>&1
+if [ $? -ne 0 ]; then
     echo "Error: Heroku login failed."
     exit 1
 else
@@ -35,7 +29,8 @@ fi
 
 # Fetch logs using Heroku API
 echo "Fetching logs from Heroku..."
-if ! heroku logs --app "$HEROKU_APP_NAME" > heroku_logs.txt 2>&1; then
+heroku logs --app "$HEROKU_APP_NAME" > heroku_logs.txt 2>&1
+if [ $? -ne 0 ]; then
     echo "Error: Failed to fetch logs from Heroku. Check heroku_logs.txt for details."
     exit 1
 else
