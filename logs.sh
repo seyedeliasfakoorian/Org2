175f35513974919dd105a5537be792a7f78a37ef
Elias Fakoorian <98291494+seyedeliasfakoorian@users.noreply.github.com>
2023-12-23 03:12:55 +0000

    Update logs.sh

--- logs.sh
+++ logs.sh
@@ -6,6 +6,9 @@ if ! command -v heroku &> /dev/null; then
     exit 1
 fi
 
+# Print all environment variables for debugging
+env
+
 # Check if required environment variables are set
 if [ -z "$HEROKU_APP_NAME" ]; then
     echo "Error: HEROKU_APP_NAME environment variable must be set."
@@ -16,6 +19,10 @@ if [ -z "$HEROKU_API_KEY" ]; then
     echo "Error: HEROKU_API_KEY environment variable must be set."
     exit 1
 fi
+
+# Print the values of HEROKU_APP_NAME and HEROKU_API_KEY for debugging
+echo "HEROKU_APP_NAME: $HEROKU_APP_NAME"
+echo "HEROKU_API_KEY: $HEROKU_API_KEY"
 
 # Log in using Heroku API key
 echo "Logging in to Heroku..."
