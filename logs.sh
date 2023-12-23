cc8d06fd7a70253c97daa2ccdc50166b49c0007d
Elias Fakoorian <98291494+seyedeliasfakoorian@users.noreply.github.com>
2023-12-22 20:24:50 -0800

    Update logs.sh

--- logs.sh
+++ logs.sh
@@ -6,29 +6,27 @@ if ! command -v heroku &> /dev/null; then
     exit 1
 fi
 
-# Print all environment variables for debugging
-env
-
 # Check if required environment variables are set
 if [ -z "$HEROKU_APP_NAME" ]; then
     echo "Error: HEROKU_APP_NAME environment variable must be set."
     exit 1
 fi
 
-# Set Heroku API key for authentication
 if [ -z "$HEROKU_API_KEY" ]; then
     echo "Error: HEROKU_API_KEY environment variable must be set."
     exit 1
 fi
 
-# Set the Heroku API key for authentication
+# Decode Heroku API key for authentication
 HEROKU_API_KEY_DECODED=$(echo "$HEROKU_API_KEY" | base64 --decode)
+
+# Create .netrc file for Heroku API key
 echo -e "machine api.heroku.com\n  login $HEROKU_API_KEY_DECODED" > ~/.netrc
 chmod 600 ~/.netrc
 
 # Fetch logs using Heroku API
-echo "Fetching logs from Heroku..."
+echo "Fetching logs from Heroku for app: $HEROKU_APP_NAME..."
 if ! heroku logs --app "$HEROKU_APP_NAME"; then
-    echo "Error: Failed to fetch logs from Heroku."
+    echo "Error: Failed to fetch logs from Heroku for app: $HEROKU_APP_NAME."
     exit 1
 fi
