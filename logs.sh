4bb6db3ff9a1a5047685ca36becd1b2e623141e5
Elias Fakoorian <98291494+seyedeliasfakoorian@users.noreply.github.com>
2023-12-23 01:57:20 +0000

    Update logs.sh

--- logs.sh
+++ logs.sh
@@ -1,2 +1,9 @@
+# Heroku App Name
 HEROKU_APP_NAME='website-breaker-demo'
+
+# Set Heroku API key for authentication
+HEROKU_API_KEY=$(cat secrets.HEROKU_API_KEY)
+echo $HEROKU_API_KEY | heroku login --interactive
+
+# Fetch logs using Heroku API
 heroku logs --app website-breaker-demo
