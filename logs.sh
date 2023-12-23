49dd65a236077803bda7676c2467cd7aad714ceb
Elias Fakoorian <98291494+seyedeliasfakoorian@users.noreply.github.com>
2023-12-23 02:03:23 +0000

    Update Org2.yml and logs.sh

--- logs.sh
+++ logs.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 # Heroku App Name
 HEROKU_APP_NAME='website-breaker-demo'
 
 # Set Heroku API key for authentication
-HEROKU_API_KEY=$(cat secrets.HEROKU_API_KEY)
+HEROKU_API_KEY=$(echo $HEROKU_API_KEY_SECRET | base64 --decode)
 echo $HEROKU_API_KEY | heroku login --interactive
 
 # Fetch logs using Heroku API
