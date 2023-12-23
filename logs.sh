5d95b95a45231c06ccc62ddb81b7533a4402154f
Elias Fakoorian <98291494+seyedeliasfakoorian@users.noreply.github.com>
2023-12-23 02:07:59 +0000

    Update logs.sh

--- logs.sh
+++ logs.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
-# Heroku App Name
-HEROKU_APP_NAME='website-breaker-demo'
+# Check if required environment variables are set
+if [ -z "$HEROKU_APP_NAME" ] || [ -z "$HEROKU_API_KEY_SECRET" ]; then
+    echo "Error: HEROKU_APP_NAME and HEROKU_API_KEY_SECRET environment variables must be set."
+    exit 1
+fi
 
 # Set Heroku API key for authentication
-HEROKU_API_KEY=$(echo $HEROKU_API_KEY_SECRET | base64 --decode)
-echo $HEROKU_API_KEY | heroku login --interactive
+HEROKU_API_KEY=$(echo "$HEROKU_API_KEY_SECRET" | base64 --decode)
+
+# Extract email and password from API key
+HEROKU_EMAIL=$(echo "$HEROKU_API_KEY" | cut -d: -f1)
+HEROKU_PASSWORD=$(echo "$HEROKU_API_KEY" | cut -d: -f2)
+
+# Log in using Heroku API key
+echo "Logging in to Heroku..."
+echo -e "${HEROKU_EMAIL}\n${HEROKU_PASSWORD}" | heroku login --interactive
+
+# Check if the login was successful
+if [ $? -ne 0 ]; then
+    echo "Error: Heroku login failed."
+    exit 1
+else
+    echo "Heroku login successful."
+fi
 
 # Fetch logs using Heroku API
-heroku logs --app website-breaker-demo
+echo "Fetching logs from Heroku..."
+heroku logs --app "$HEROKU_APP_NAME"
