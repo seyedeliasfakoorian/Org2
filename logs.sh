ba79fd80136f8ff040d4fb6fa6003f1a856c96be
Elias Fakoorian <98291494+seyedeliasfakoorian@users.noreply.github.com>
2023-12-26 17:48:18 -0800

    Update logs.sh

--- logs.sh
+++ logs.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# Function to check if a required environment variable is set
+check_env_variable() {
+    if [ -z "${!1}" ]; then
+        echo "Error: $1 environment variable must be set."
+        exit 1
+    fi
+}
+
 # Check if the 'heroku' command is available
 if ! command -v heroku &> /dev/null; then
     echo "Error: 'heroku' command not found. Please make sure it's installed."
@@ -7,27 +15,19 @@ if ! command -v heroku &> /dev/null; then
 fi
 
 # Check if required environment variables are set
-if [ -z "$secrets.HEROKU_APP_NAME" ]; then
-    echo "Error: 'secrets.HEROKU_APP_NAME' environment variable must be set."
-    exit 1
-fi
-
-if [ -z "$secrets.HEROKU_API_KEY" ]; then
-    echo "Error: 'secrets.HEROKU_API_KEY' environment variable must be set."
-    exit 1
-fi
+check_env_variable "secrets.HEROKU_APP_NAME"
+check_env_variable "secrets.HEROKU_API_KEY"
 
 # Decode Heroku API key for authentication
-HEROKU_API_KEY_DECODED=$(echo "$secrets.HEROKU_API_KEY" | base64 --decode)
+HEROKU_API_KEY_DECODED=$(echo "${secrets.HEROKU_API_KEY}" | base64 --decode)
 
 # Login to Heroku with .netrc
 echo -e "machine api.heroku.com\n  login $HEROKU_API_KEY_DECODED" > ~/.netrc
 chmod 600 ~/.netrc
 
 # Fetch logs using Heroku API
-APP_NAME="$secrets.HEROKU_APP_NAME"
-echo "Fetching logs from Heroku for app: $APP_NAME..."
-if ! heroku logs --app "$APP_NAME"; then
-    echo "Error: Failed to fetch logs from Heroku for app: $APP_NAME"
+echo "Fetching logs from Heroku for app: website-breaker-demo..."
+if ! heroku logs --app website-breaker-demo; then
+    echo "Error: Failed to fetch logs from Heroku for app: website-breaker-demo"
     exit 1
 fi
