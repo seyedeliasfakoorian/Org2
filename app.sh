aab8295a7b06d7a05bb5a1f46a93168a8bc44a47
Elias Fakoorian <98291494+seyedeliasfakoorian@users.noreply.github.com>
2024-03-23 16:30:19 -0700

    Update app.sh

--- app.sh
+++ app.sh
@@ -34,14 +34,7 @@ HEROKU_API_KEY_DECODED=$(echo "$HEROKU_API_KEY" | base64 --decode)
 echo "Fetching logs from Heroku for app: $HEROKU_APP_NAME..."
 if ! HEROKU_API_KEY="$HEROKU_API_KEY_DECODED" heroku logs --app "$HEROKU_APP_NAME"; then
     echo "Error: Failed to fetch logs from Heroku for app: $HEROKU_APP_NAME"
-    
-    # Additional steps to create a pull request on GitHub for fixing log fetching failure
-    echo "Creating a pull request for Heroku log fetching failure..."
-    
-    # Assuming 'gh' command-line tool is installed and configured
-    gh pr create --base main --head fix-heroku-log-fetching --title "Fix Heroku log fetching failure" --body "This pull request can fix log fetching issues when Heroku has an issue deploying or needs to check logs🚀
-    🌎 [Heroku App](https://$HEROKU_APP_NAME.herokuapp.com/)" \
-    --base main --branch github-actions --commit-message "Fix Heroku Log Fetching Failure" --delete-branch true --labels "github actions" --reviewers seyedeliasfakoorian
+    exit 1
 fi
 
 # Make a request to Heroku API to get release status
@@ -50,16 +43,10 @@ HEROKU_DEPLOYMENT_STATUS=$(curl -n -s "$HEROKU_RELEASES_API_URL" \
   -H "Authorization: Bearer $HEROKU_API_KEY_DECODED" \
   | jq -r 'if type == "array" then .[0].status else "succeeded" end')
 
-# Print Heroku Log status
+# Print Heroku Deployment status
 echo "Heroku Deployment Status: $HEROKU_DEPLOYMENT_STATUS"
 
 # Check if deployment failed
 if [ "$HEROKU_DEPLOYMENT_STATUS" != "succeeded" ]; then
-  # Additional steps to create a pull request on GitHub for fixing Heroku deployment failure
-  echo "Creating a pull request for Heroku deployment failure..."
-  
-  # Assuming 'gh' command-line tool is installed and configured
-  gh pr create --base main --head fix-heroku-deployment --title "Fix Heroku deployment failure" --body "This pull request can fix deployment issues when Heroku has an issue deploying or needs to check logs🚀
-  🌎 [Heroku App](https://$HEROKU_APP_NAME.herokuapp.com/)" \
-  --base main --branch github-actions --commit-message "Fix Heroku Deployment Failure" --delete-branch true --labels "github actions" --reviewers seyedeliasfakoorian
+  echo "Heroku deployment failed."
 fi
